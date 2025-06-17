e4e2b4d5c44e10bdd6d9caaa6899d585a07ba08d
maurizio-dominici <mauriziodominici4@gmail.com>
2025-06-17 14:58:21 +0200

    fix controller

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -28,8 +28,25 @@ const show = (req, res) => {
         message: "movie not found",
       });
 
-    res.json({
-      movie: results,
+    const movie = results[0];
+    const reviewsSql = "SELECT * FROM reviews WHERE movie_id = ?";
+
+    connection.query(reviewsSql, [id], (err, results) => {
+      if (err)
+        return res.status(500).json({
+          message: "server problem",
+        });
+
+      if (!results.length)
+        return res.status(404).json({
+          message: "movie not found",
+        });
+
+      movie.reviews = results;
+
+      res.json({
+        movie,
+      });
     });
   });
 };
